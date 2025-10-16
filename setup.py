19f2f46000ea8de4be572cc98d72666ec4cde481
Tim Vos <73369902+Sjakiesurfer1984@users.noreply.github.com>
2025-10-16 12:06:08 +0800

    Update version to 0.1.1 in setup.py
    Bump version to 0.1.1 due to updated dependencies.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Exploratory_Data_Analysis_Suite",
-    version="0.1",
+    version="0.1.1",  # bump version slightly since dependencies changed
     packages=find_packages(),
     install_requires=[
         "dependency-injector>=4.41.0",
@@ -10,5 +10,6 @@ setup(
         "matplotlib>=3.4.0",
         "seaborn>=0.11.0",
         "numpy>=1.20.0",
+        "python-docx>=0.8.11",
     ],
 )
